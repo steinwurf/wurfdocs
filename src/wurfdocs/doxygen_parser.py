f84cc2e9b22d7f45dd7a410b5fe1f5bcae91820b
Morten V. Pedersen <morten@mortenvp.com>
2018-04-24 23:17:35 +0200

    Working on it

--- src/wurfdocs/doxygen_parser.py
+++ src/wurfdocs/doxygen_parser.py
@@ -207,7 +207,7 @@ class DoxygenParser(object):
 
             else:
                 kind = element.attr('kind')
-                self.log.warning('Not supported {}'.format(kind))
+                self.log.warning('Not supported {}'.format(kind), color=None)
 
         return api
 
